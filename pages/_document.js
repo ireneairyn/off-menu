f06cc98e3efc34f2e3f71f0b948a6ae7dd86c798
Irene Aunon <irene@ireneaunion.com>
2023-04-24 00:50:39 +0200

    create very basic structure of web and form page

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Bungee+Inline&family=Saira:wght@800&display=swap"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
---
+++ pages/form.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+function FormPage() {
+  return (
+    <>
+      <Head>
+        <title>Off Menu Dream Meal Generator</title>
+      </Head>
+      <div>
+        <h1>Form Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default FormPage
