df3a805a713acdb31f68c7e1d081bd9c1daf2fc3
Irene Aunon <irene@ireneaunion.com>
2023-04-25 02:38:21 +0200

    style men page

--- components/Menu.js
+++ components/Menu.js
@@ -5,15 +5,23 @@ function Menu(props) {
     props;
 
   return (
-    <div>
-      <h2>Your Meal:</h2>
+    <div className="finalMenuContainer">
+      <div className="finalMenu">
+      <h2>Your Dream Meal</h2>
       <p>{water}</p>
       <p>{poppabread}</p>
-      <p>Starter: {starter}</p>
-      <p>Main Course: {mainCourse}</p>
-      <p>Side Dish: {sideDish}</p>
-      <p>Drink: {drink}</p>
-      <p>Dessert: {dessert}</p>
+    
+      <p className="dish">~ Starter ~</p>
+      <p>{starter}</p>
+      <p className="dish">~ Main Course ~</p>
+      <p>{mainCourse}</p>
+      <p className="dish">~ Side Dish ~</p>
+      <p>{sideDish}</p>
+      <p className="dish">~ Drink ~</p>
+      <p>{drink}</p>
+      <p className="dish">~ Dessert ~</p>
+      <p>{dessert}</p>
+    </div>
     </div>
   );
 }
