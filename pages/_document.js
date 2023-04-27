9247ddd40c1f4d402a6a7e3961aeecbdd1c83716
Irene Aunon <irene@ireneaunion.com>
2023-04-27 23:09:58 +0200

    add OGimage

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bungee+Inline&family=Bungee+Shade&family=Caveat+Brush&family=Princess+Sofia&family=Reem+Kufi&display=swap"
           />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <meta property="og:image" content="/OGimage.png" />
+          <meta property="og:title" content="Off Menu" />
+          <meta property="og:description" content="Dream Meal Generator" />
         </Head>
         <body>
           <Main />
